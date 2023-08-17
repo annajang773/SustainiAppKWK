cdb1aaf88b7ce9cdbbd0d0bd5becf47d4052d028
annajang <annajang1234@gmail.com>
2023-08-17 09:21:54 -0400

    Initial Commit

---
+++ SustainiAppKWK/SustainiAppKWKApp.swift
@@ -0,0 +1,17 @@
+//
+//  SustainiAppKWKApp.swift
+//  SustainiAppKWK
+//
+//  Created by scholar on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SustainiAppKWKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
